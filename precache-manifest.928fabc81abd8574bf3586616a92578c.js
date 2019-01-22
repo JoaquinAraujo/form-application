86e02795f9435983bd73c4ed2a598b891409db21
Joaquin Araujo <joaquinaraujojs@gmail.com>
2019-01-21 23:20:48 -0400

    deploy

---
+++ precache-manifest.928fabc81abd8574bf3586616a92578c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "006a49646cb744f96f9b",
+    "url": "/static/js/main.006a4964.chunk.js"
+  },
+  {
+    "revision": "dc6fe4194dc2bc9c78fd",
+    "url": "/static/js/1.dc6fe419.chunk.js"
+  },
+  {
+    "revision": "006a49646cb744f96f9b",
+    "url": "/static/css/main.c2d87661.chunk.css"
+  },
+  {
+    "revision": "29778048299a8bf48bcd72d9fc6e6780",
+    "url": "/index.html"
+  }
+];
